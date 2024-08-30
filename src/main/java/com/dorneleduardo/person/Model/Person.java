ff7a38451d2d955b7dbc06e6358e3e428bfaf7ad
Eduardo Dornel <dornel.eduardo@gmail.com>
2024-08-29 23:41:35 -0300

    Add HTTP verbs in the application

--- src/main/java/com/dorneleduardo/person/Model/Person.java
+++ src/main/java/com/dorneleduardo/person/Model/Person.java
@@ -1,29 +1,58 @@
 package com.dorneleduardo.person.Model;
 
+import jakarta.persistence.*;
+
 import java.io.Serializable;
 
+@Entity
+@Table(name = "person")
 public class Person implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "first_name", nullable = false, length = 80)
     private String firstName;
+
+    @Column(name = "last_name" , nullable = false, length = 80)
     private String lastName;
+
+    @Column(nullable = false, length = 100)
     private String address;
+
+    @Column(nullable = false, length = 6)
     private String gender;
 
-    public void setId(Long id) {
-        this.id = id;
+
+    public String getFirstName() {
+        return firstName;
     }
 
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
 
+    public String getLastName() {
+        return lastName;
+    }
+
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
 
+    public String getAddress() {
+        return address;
+    }
+
     public void setAddress(String address) {
         this.address = address;
+    }
+
+
+
+    public String getGender() {
+        return gender;
     }
 
     public void setGender(String gender) {
@@ -33,31 +62,4 @@ public class Person implements Serializable {
     public Long getId() {
         return id;
     }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public String getGender() {
-        return gender;
-    }
-
-    public Person() {
-
-
-
-
-
-    }
-
-
-
 }
--- src/main/java/com/dorneleduardo/person/Services/PersonServices.java
+++ src/main/java/com/dorneleduardo/person/Services/PersonServices.java
@@ -1,35 +1,48 @@
 package com.dorneleduardo.person.Services;
 
+import com.dorneleduardo.person.Exceptions.ResourceNotFoundException;
 import com.dorneleduardo.person.Model.Person;
+import com.dorneleduardo.person.Repositories.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Logger;
 
 @Service
 public class PersonServices {
 
-    private final AtomicLong counter = new AtomicLong();
     private Logger logger = Logger.getLogger(PersonServices.class.getName());
 
+    @Autowired
+    PersonRepository repository;
 
     public Person create(Person person){
         logger.info("Adding new person");
-        return person;
+        return repository.save(person);
 
     }
 
     public Person update(Person person){
         logger.info("Updating person");
-        return person;
+
+       var entity =  repository.findById(person.getId()).orElseThrow(() -> new ResourceNotFoundException("Person not Found"));
+
+       entity.setFirstName(person.getFirstName());
+       entity.setLastName(person.getLastName());
+       entity.setAddress(person.getAddress());
+       entity.setGender(person.getGender());
+
+        return repository.save(person);
 
     }
 
-    public void delete(String id){
-        logger.info("Deleting person");
+    public void delete(Long id){
 
+        logger.info("Deleting person");
+        var entity =  repository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Person not Found"));
+    repository.delete(entity);
 
     }
 
@@ -39,44 +52,19 @@ public class PersonServices {
 
         logger.info("Finding all people");
         List<Person> persons = new ArrayList<>();
-        for (int i = 0; i < 8; i++){
 
-            Person person = mockPerson(i);
-            persons.add(person);
-
-        }
-        return persons;
+        return repository.findAll();
 
 
     }
 
-    private Person mockPerson(int i) {
 
 
-        logger.info("Finding one person");
-
-        Person person = new Person();
-        person.setId(counter.incrementAndGet());
-        person.setFirstName("Person name" + i);
-        person.setLastName("Person last name" + i);
-        person.setAddress("Paper Street - 33");
-        person.setGender("Male");
-        return person;
-
-
-    }
-
-    public Person findById(String id){
+    public Person findById(Long id){
 
         logger.info("Finding one person");
 
-        Person person = new Person();
-        person.setId(counter.incrementAndGet());
-        person.setFirstName("Eduardo");
-        person.setLastName("Dornel");
-        person.setAddress("Paper Street - 33");
-        person.setGender("Male");
-        return person;
+        return repository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Person not Found"));
 
     }
 
